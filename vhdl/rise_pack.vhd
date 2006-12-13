1f61bfd3ebe3debc30a0cae1922939ed68f25e39
cwalter <cwalter@73358e6e-dbde-4195-9c26-2959edb4864b>
2006-12-13 02:05:05 +0000

    - correct register address width is 4 bit and not 5 bit.  - added constants for OPCODES, COND and SR.

--- vhdl/rise_pack.vhd
+++ vhdl/rise_pack.vhd
@@ -27,7 +27,7 @@ package RISE_PACK is
   constant MEM_ADDR_WIDTH : integer := ARCHITECTURE_WIDTH;
   
   constant REGISTER_WIDTH : integer := ARCHITECTURE_WIDTH;
-  constant REGISTER_ADDR_WIDTH : integer := 5;
+  constant REGISTER_ADDR_WIDTH : integer := 4;
   constant IMMEDIATE_WIDTH : integer := ARCHITECTURE_WIDTH;
   constant LOCK_WIDTH : integer := REGISTER_COUNT;
   
@@ -45,6 +45,23 @@ package RISE_PACK is
   subtype OPCODE_T is std_logic_vector(OPCODE_WIDTH-1 downto 0);
   subtype COND_T is std_logic_vector(COND_WIDTH-1 downto 0);
 
+  --
+  constant SR_REGISTER_DI : INTEGER := 15;
+  constant SR_REGISTER_IP_MASK : INTEGER := 12;
+  constant SR_REGISTER_OVERFLOW : INTEGER := 3;
+  constant SR_REGISTER_NEGATIVE : INTEGER := 2;
+  constant SR_REGISTER_CARRY : INTEGER := 1;
+  constant SR_REGISTER_ZERO : INTEGER := 0;
+  constant RESET_PC_VALUE : PC_REGISTER_T := ( others => '0' );
+  constant RESET_SR_VALUE : PC_REGISTER_T := ( others => '0' );
+
+  constant COND_NONE : COND_T := "000";
+  -- RISE OPCODES --
+  constant OPCODE_LD_IMM : OPCODE_T := "10000";
+  constant OPCODE_LD_DISP : OPCODE_T := "10100";
+  constant OPCODE_LD_DISP_MS : OPCODE_T := "11000";
+  constant OPCODE_LD_REG : OPCODE_T := "00001";
+  constant OPCODE_NOP : OPCODE_T := "00000";
     
   type IF_ID_REGISTER_T is record
                              pc : PC_REGISTER_T;
