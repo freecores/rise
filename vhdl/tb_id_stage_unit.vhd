aa9b0b6c44c06e3a47bb9de67b142287648f6932
cwalter <cwalter@73358e6e-dbde-4195-9c26-2959edb4864b>
2007-01-03 02:13:42 +0000

    - Testbench now implements a simple register file.  - Added new tests for OPCODE_LD_DISP, OPCODE_LD_DISP_MS    OPCODE_LD_REG.

--- vhdl/tb_id_stage_unit.vhd
+++ vhdl/tb_id_stage_unit.vhd
@@ -51,8 +51,8 @@ architecture behavior of tb_id_stage_unit_vhd is
   signal reset          : std_logic := '0';
   signal if_id_register : IF_ID_REGISTER_T;
 
-  signal stall_in       : std_logic     := '0';
-  signal clear_in       : std_logic     := '0';
+  signal stall_in       : std_logic := '0';
+  signal clear_in       : std_logic := '0';
   signal rx             : REGISTER_T;
   signal ry             : REGISTER_T;
   signal rz             : REGISTER_T;
@@ -67,13 +67,15 @@ architecture behavior of tb_id_stage_unit_vhd is
   signal lock_reg_addr  : std_logic_vector(REGISTER_ADDR_WIDTH - 1 downto 0);
   signal stall_out      : std_logic;
 
-  constant TB_RX_ADDR_TEST_VALUE : REGISTER_ADDR_T := x"1";
-  constant TB_RX_TEST_VALUE : REGISTER_T := x"0001";
-  constant TB_RY_TEST_VALUE : REGISTER_T := x"0002";
-  constant TB_RZ_TEST_VALUE : REGISTER_T := x"0003";
+  constant TB_COND_TEST_VALUE : COND_T := COND_NONE;
+
+  constant TB_R1_TEST_VALUE : REGISTER_T := x"0001";
+  constant TB_R2_TEST_VALUE : REGISTER_T := x"0002";
+  constant TB_R3_TEST_VALUE : REGISTER_T := x"0003";
+
   constant TB_SR_TEST_VALUE : SR_REGISTER_T := x"A55A";
   constant TB_PC_TEST_VALUE : SR_REGISTER_T := x"1234";
-  
+
   constant TB_CLOCK : time := 20 ns;
 begin
 
@@ -106,6 +108,33 @@ begin
     wait for TB_CLOCK/2;
   end process;
 
+  regfile : process(rx_addr, ry_addr, rz_addr)
+  begin
+    case rx_addr is
+      when x"1"   => rx <= TB_R1_TEST_VALUE;
+      when x"2"   => rx <= TB_R2_TEST_VALUE;
+      when x"3"   => rx <= TB_R3_TEST_VALUE;
+      when others => rz <= (others => 'X');
+    end case;
+
+    case ry_addr is
+      when x"1"   => ry <= TB_R1_TEST_VALUE;
+      when x"2"   => ry <= TB_R2_TEST_VALUE;
+      when x"3"   => ry <= TB_R3_TEST_VALUE;
+      when others => rz <= (others => 'X');
+    end case;
+
+    case rz_addr is
+      when x"1"   => rz <= TB_R1_TEST_VALUE;
+      when x"2"   => rz <= TB_R2_TEST_VALUE;
+      when x"3"   => rz <= TB_R3_TEST_VALUE;
+      when others => rz <= (others => 'X');
+    end case;
+
+    sr <= TB_SR_TEST_VALUE;
+    
+  end process;
+
   tb : process
   begin
     reset <= '0';
@@ -114,33 +143,75 @@ begin
 
     -- test case: basic functionallity
     if_id_register.pc <= TB_PC_TEST_VALUE;
-    sr <= TB_SR_TEST_VALUE;
-    rx <= TB_RX_TEST_VALUE;
-    ry <= TB_RY_TEST_VALUE;
-    rz <= TB_RZ_TEST_VALUE;
-    
     wait for TB_CLOCK;
-    
+
     -- test case: OPCODE_LD_IMM 
-    if_id_register.ir <= "100"& "0" & TB_RX_ADDR_TEST_VALUE & x"55";
+    if_id_register.ir <= "100"& "0" & x"1" & x"55";
     wait for TB_CLOCK;
     assert id_ex_register.opcode = OPCODE_LD_IMM;
     assert id_ex_register.immediate = x"0055";
     assert id_ex_register.cond = COND_UNCONDITIONAL;
-    assert rx_addr = TB_RX_ADDR_TEST_VALUE;
-    assert id_ex_register.rX_addr = TB_RX_ADDR_TEST_VALUE;
-    assert id_ex_register.rX = TB_RX_TEST_VALUE;
+    assert rx_addr = x"1";
+    assert id_ex_register.rX_addr = x"1";
+    assert id_ex_register.rX = TB_R1_TEST_VALUE;
 
     -- test case: OPCODE_LD_IMM_HB
-    if_id_register.ir <= "100"& "1" & TB_RX_ADDR_TEST_VALUE & x"55";
+    if_id_register.ir <= "100"& "1" & x"1" & x"55";
     wait for TB_CLOCK;
     assert id_ex_register.opcode = OPCODE_LD_IMM_HB;
     assert id_ex_register.immediate = x"5500";
     assert id_ex_register.cond = COND_UNCONDITIONAL;
-    assert rx_addr = TB_RX_ADDR_TEST_VALUE;
-    assert id_ex_register.rX_addr = TB_RX_ADDR_TEST_VALUE;
-    assert id_ex_register.rX = TB_RX_TEST_VALUE;
-    
+    assert rx_addr = x"1";
+    assert id_ex_register.rX_addr = x"1";
+    assert id_ex_register.rX = TB_R1_TEST_VALUE;
+
+
+    -- test case: OPCODE_LD_IMM_HB
+    if_id_register.ir <= "100"& "1" & x"1" & x"55";
+    wait for TB_CLOCK;
+    assert id_ex_register.opcode = OPCODE_LD_IMM_HB;
+    assert id_ex_register.immediate = x"5500";
+    assert id_ex_register.cond = COND_UNCONDITIONAL;
+    assert rx_addr = x"1";
+    assert id_ex_register.rX_addr = x"1";
+    assert id_ex_register.rX = TB_R1_TEST_VALUE;
+
+    -- test case: OPCODE_LD_DISP
+    if_id_register.ir <= "101"& "000" & "11" & x"1" & x"2";
+    wait for TB_CLOCK;
+    assert id_ex_register.opcode = OPCODE_LD_DISP;
+    assert id_ex_register.rX_addr = x"1";
+    assert id_ex_register.rX = TB_R1_TEST_VALUE;
+    assert id_ex_register.rY = TB_R2_TEST_VALUE;
+    assert id_ex_register.rZ = TB_R3_TEST_VALUE;
+    assert id_ex_register.cond = TB_COND_TEST_VALUE;
+    assert rx_addr = x"1";
+    assert ry_addr = x"2";
+    assert rz_addr = x"3";
+
+    -- test case: OPCODE_LD_DISP_MS
+    if_id_register.ir <= "110" & "000" & "11" & x"1" & x"2";
+    wait for TB_CLOCK;
+    assert id_ex_register.opcode = OPCODE_LD_DISP_MS;
+    assert id_ex_register.rX_addr = x"1";
+    assert id_ex_register.rX = TB_R1_TEST_VALUE;
+    assert id_ex_register.rY = TB_R2_TEST_VALUE;
+    assert id_ex_register.rZ = TB_R3_TEST_VALUE;
+    assert id_ex_register.cond = TB_COND_TEST_VALUE;
+    assert rx_addr = x"1";
+    assert ry_addr = x"2";
+    assert rz_addr = x"3";
+
+    -- test case: OPCODE_LD_REG
+    if_id_register.ir <= "00001" & "001" & x"2" & x"1";
+    wait for TB_CLOCK;
+    assert id_ex_register.opcode = OPCODE_LD_REG;
+    assert id_ex_register.rX_addr = x"2";
+    assert id_ex_register.rX = TB_R2_TEST_VALUE;
+    assert id_ex_register.rY = TB_R1_TEST_VALUE;
+    assert id_ex_register.cond = COND_NOT_ZERO;
+    assert rx_addr = x"2";
+    assert ry_addr = x"1";
     wait;                               -- will wait forever
   end process;
 
