76a3d99cfb7b3e9f23ad0f3c8be86748754ed711
ustadler <ustadler@73358e6e-dbde-4195-9c26-2959edb4864b>
2007-01-10 03:06:51 +0000

    4k Data Memory for Spartan 3 (Block RAM) Added write enable to the entity

--- vhdl/dmem.vhd
+++ vhdl/dmem.vhd
@@ -1,3 +1,4 @@
+
 -- File: dmem.vhd
 -- Author: Jakob Lechner, Urban Stadler, Harald Trinkl, Christian Walter
 -- Created: 2006-11-29
@@ -20,6 +21,7 @@ entity dmem is
     clk            : in std_logic;
     reset          : in std_logic;
 
+    wr_enable		 : in std_logic;
     addr           : in MEM_ADDR_T;
     data_in        : in MEM_DATA_T;
     data_out       : out MEM_DATA_T);
@@ -28,8 +30,34 @@ end dmem;
 
 architecture dmem_rtl of dmem is
 
+component idmem
+	port (
+	addr: IN std_logic_VECTOR(11 downto 0);
+	clk: IN std_logic;
+	din: IN std_logic_VECTOR(15 downto 0);
+	dout: OUT std_logic_VECTOR(15 downto 0);
+	sinit: IN std_logic;
+	we: IN std_logic);
+end component;
+
 begin  -- dmem_rtl
 
+DATA_MEM : idmem
+	port map (
+	addr => addr(11 downto 0),
+	clk => clk,
+	din => data_in,
+	dout => data_out,
+	sinit => reset,
+	we => wr_enable);
   
 
 end dmem_rtl;
+
+
+
+
+
+
+
+
