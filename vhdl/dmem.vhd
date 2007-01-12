360a1f50b9cb315f6a7b1743813847e3e82ed597
jlechner <jlechner@73358e6e-dbde-4195-9c26-2959edb4864b>
2007-01-12 23:00:01 +0000

    Applied VHDL indent.

--- vhdl/dmem.vhd
+++ vhdl/dmem.vhd
@@ -20,8 +20,7 @@ entity dmem is
   port (
     clk            : in std_logic;
     reset          : in std_logic;
-
-    wr_enable		 : in std_logic;
+    wr_enable	   : in std_logic;
     addr           : in MEM_ADDR_T;
     data_in        : in MEM_DATA_T;
     data_out       : out MEM_DATA_T);
@@ -30,26 +29,26 @@ end dmem;
 
 architecture dmem_rtl of dmem is
 
-component idmem
-	port (
-	addr: IN std_logic_VECTOR(11 downto 0);
-	clk: IN std_logic;
-	din: IN std_logic_VECTOR(15 downto 0);
-	dout: OUT std_logic_VECTOR(15 downto 0);
-	sinit: IN std_logic;
-	we: IN std_logic);
-end component;
+  component idmem
+    port (
+      addr: IN std_logic_VECTOR(11 downto 0);
+      clk: IN std_logic;
+      din: IN std_logic_VECTOR(15 downto 0);
+      dout: OUT std_logic_VECTOR(15 downto 0);
+      sinit: IN std_logic;
+      we: IN std_logic);
+  end component;
 
 begin  -- dmem_rtl
 
-DATA_MEM : idmem
-	port map (
-	addr => addr(11 downto 0),
-	clk => clk,
-	din => data_in,
-	dout => data_out,
-	sinit => reset,
-	we => wr_enable);
+  DATA_MEM : idmem
+    port map (
+      addr => addr(11 downto 0),
+      clk => clk,
+      din => data_in,
+      dout => data_out,
+      sinit => reset,
+      we => wr_enable);
   
 
 end dmem_rtl;
