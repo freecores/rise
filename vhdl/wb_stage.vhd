a0aa83be43b051bcf3e43318258dc9e372c23308
cwalter <cwalter@73358e6e-dbde-4195-9c26-2959edb4864b>
2007-01-14 17:55:45 +0000

    - Computed new SR values from ALU result.

--- vhdl/wb_stage.vhd
+++ vhdl/wb_stage.vhd
@@ -49,6 +49,7 @@ begin  -- wb_stage_rtl
   clear_out <= '0';  -- clear_out output is unused at the moment.
 
   process (reset, mem_wb_register)
+    variable sr_test_value : REGISTER_T;
   begin
     if reset = '0' then
       clear_reg_lock0 <= '0';
@@ -96,9 +97,22 @@ begin  -- wb_stage_rtl
         lr              <= ( others => 'X' );
         lr_enable       <= '0';
       end if;
+      
       -- write back of SR --
       if mem_wb_register.aluop2(ALUOP2_SR_BIT) = '1' then
+        -- calculate SR value
         sr              <= mem_wb_register.sr;
+        if mem_wb_register.aluop1(ALUOP1_LD_MEM_BIT) = '1' then
+          sr_test_value := mem_wb_register.mem_reg;
+        else
+          sr_test_value := mem_wb_register.reg;
+        end if;
+        if sr_test_value = CONV_STD_LOGIC_VECTOR(0, REGISTER_WIDTH) then
+          sr( SR_REGISTER_ZERO ) <= '1';
+        end if;
+        if sr_test_value( REGISTER_WIDTH - 1 ) = '1' then
+          sr( SR_REGISTER_NEGATIVE ) <= '1';
+        end if;
         sr_enable       <= '1';
         clear_reg_lock1 <= '1';
         lock_reg_addr1  <= SR_REGISTER_ADDR;
@@ -106,6 +120,8 @@ begin  -- wb_stage_rtl
         sr              <= ( others => 'X' );
         sr_enable       <= '0';
       end if;
+      
+      
     end if;
   end process;
 end wb_stage_rtl;
